f13c59854ca3f5034d5b0436044ce1a98b7839cd
Florian Zimmermeister <flozi00.fz@gmail.com>
2022-12-04 16:29:33 +0100

    different model sizes

--- aaas/statics.py
+++ aaas/statics.py
@@ -2,7 +2,19 @@ from transformers import AutoModelForSpeechSeq2Seq
 from optimum.onnxruntime import ORTModelForSpeechSeq2Seq
 
 MODEL_MAPPING = {
-    "german": {"name": "flozi00/whisper-small-german", "class": ORTModelForSpeechSeq2Seq},
-    "universal": {"name": "openai/whisper-large", "class": ORTModelForSpeechSeq2Seq},
+    "small":{
+        "german": {"name": "flozi00/whisper-small-german", "class": ORTModelForSpeechSeq2Seq},
+        "english": {"name": "openai/whisper-small.en", "class": ORTModelForSpeechSeq2Seq},
+        "universal": {"name": "openai/whisper-small", "class": ORTModelForSpeechSeq2Seq},
+    },
+    "medium":{
+        "german": {"name": "flozi00/whisper-medium-german", "class": ORTModelForSpeechSeq2Seq},
+        "english": {"name": "openai/whisper-medium.en", "class": ORTModelForSpeechSeq2Seq},
+        "universal": {"name": "openai/whisper-medium", "class": ORTModelForSpeechSeq2Seq},
+    },
+    "large":{
+        "german": {"name": "flozi00/whisper-large-german", "class": ORTModelForSpeechSeq2Seq},
+        "universal": {"name": "openai/whisper-large", "class": ORTModelForSpeechSeq2Seq},
+    },
 }
 LANG_MAPPING = {"german": "de", "english": "en", "french": "fr", "spanish": "es"}
