1ba7b262a6f1b47e659a26f60feaad9fcaa0636f
Florian Zimmermeister <flozi00.fz@gmail.com>
2022-12-06 12:38:28 +0100

    fix cloudrun

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from aaas.gradio_utils import build_gradio
 if __name__ == "__main__":
     ui = build_gradio()
     ui.queue()
-    ui.launch(server_name="0.0.0.0", server_port=os.getenv("PORT", 7860))
+    ui.launch(server_name="0.0.0.0", server_port=int(os.getenv("PORT", 7860)))
