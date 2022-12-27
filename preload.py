078d68f587acfa5340069e1b50eb67fe97f495e6
Florian Zimmermeister <flozi00.fz@gmail.com>
2022-12-27 14:38:25 +0100

    clean repo

--- preload.py
+++
@@ -1,6 +0,0 @@
-from aaas.audio_utils import get_model_and_processor
-from aaas.statics import MODEL_MAPPING
-
-for mode in ["small", "medium", "large"]:
-    for model in list(MODEL_MAPPING[mode].keys()):
-        get_model_and_processor(model, mode)
