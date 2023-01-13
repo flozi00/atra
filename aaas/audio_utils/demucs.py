cb05db1b79f41fdd2c7d3c7c954034a0f40fafa6
Florian Zimmermeister <flozi00.fz@gmail.com>
2023-01-13 19:25:34 +0100

    demucs force cpu

--- aaas/audio_utils/demucs.py
+++ aaas/audio_utils/demucs.py
@@ -7,6 +7,6 @@ demucs_model = "htdemucs"
 def seperate_vocal(audio):
     os.makedirs("out", exist_ok=True)
     sf.write("audio.wav", audio, samplerate=16000)
-    os.system(f"python -m demucs.separate -n {demucs_model} audio.wav -o out")
+    os.system(f"python -m demucs.separate -n {demucs_model} -d cpu audio.wav -o out")
     return f"./out/{demucs_model}/audio/vocals.wav"
 
