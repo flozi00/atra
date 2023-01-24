c976435eea7c992f376271f62e110c5acf126344
Florian Zimmermeister <flozi00.fz@gmail.com>
2023-01-24 13:06:22 +0100

    rm model

--- aaas/statics.py
+++ aaas/statics.py
@@ -31,10 +31,6 @@ MODEL_MAPPING = {
             },
         },
         "large": {
-            "german": {
-                "name": "flozi00/whisper-large-german",
-                "class": AutoModelForSpeechSeq2Seq,
-            },
             "universal": {
                 "name": "openai/whisper-large-v2",
                 "class": AutoModelForSpeechSeq2Seq,
