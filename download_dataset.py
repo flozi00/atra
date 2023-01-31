0afca74e14e799654fa51af077adfad97274fc7e
Florian Zimmermeister <flozi00.fz@gmail.com>
2023-01-31 11:02:03 +0100

    merge datasets

--- download_dataset.py
+++ download_dataset.py
@@ -10,20 +10,9 @@ parser.add_argument("--name")
 parser.add_argument("--url")
 parser.add_argument("--lang")
 parser.add_argument("--token")
+parser.add_argument("--merges")
 
 args = parser.parse_args()
-
-dataset_name = f"dataset/{args.name}"
-
-if os.path.isdir(dataset_name) == False:
-    vc = VCtube(dataset_name, args.url, args.lang,)
-    vc.download_audio()  # download audios from youtube
-    vc.download_captions()  # download captions from youtube
-    vc.audio_split()
-
-pds = pandas.read_csv(
-    f"./{dataset_name}/metadata.csv", sep="|", names=["audio", "text"]
-)
 
 
 def add_prefix(x):
@@ -31,14 +20,37 @@ def add_prefix(x):
     return x
 
 
-ds = datasets.Dataset.from_pandas(pds)
-print(ds)
-ds = ds.map(add_prefix)
-ds = ds.filter(lambda x: os.path.isfile(x["audio"]))
-ds = ds.cast_column("audio", datasets.Audio())
-print(ds)
+if args.merges:
+    data_sets = []
+    for d in args.merges.split(","):
+        if args.token:
+            data_sets.append(
+                datasets.load_dataset(d, split="train", use_auth_token=args.token)
+            )
+        else:
+            data_sets.append(datasets.load_dataset(d, split="train"))
+    ds = datasets.concatenate_datasets(data_sets)
+
+else:
+    dataset_name = f"dataset/{args.name}"
+    if os.path.isdir(dataset_name) == False:
+        vc = VCtube(dataset_name, args.url, args.lang,)
+        vc.download_audio()  # download audios from youtube
+        vc.download_captions()  # download captions from youtube
+        vc.audio_split()
+
+    pds = pandas.read_csv(
+        f"./{dataset_name}/metadata.csv", sep="|", names=["audio", "text"]
+    )
+
+    ds = (
+        datasets.Dataset.from_pandas(pds)
+        .map(add_prefix)
+        .filter(lambda x: os.path.isfile(x["audio"]))
+        .cast_column("audio", datasets.Audio())
+    )
 
 if args.token:
-    ds.push_to_hub(args.name, token=args.token)
+    ds.push_to_hub(args.name, token=args.token, max_shard_size="50MB")
 else:
     ds.push_to_hub(args.name, private=True)
