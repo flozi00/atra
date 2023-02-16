0f83af8c50e3567f7417ee1b4d33856fa6af0f15
Florian Zimmermeister <flozi00.fz@gmail.com>
2023-02-16 11:55:20 +0100

    req cleaning

--- aaas/model_utils.py
+++ aaas/model_utils.py
@@ -11,6 +11,7 @@ def get_model(model_class, model_id):
     model = model_class.from_pretrained(
         model_id,
         cache_dir="./model_cache",
+        device_map="auto",
     )
     try:
         model = BetterTransformer.transform(model)
