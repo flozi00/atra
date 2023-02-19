e7411ff575223e3a5a434a24f446e12ccd97d12f
Florian Zimmermeister <flozi00.fz@gmail.com>
2023-02-19 22:05:05 +0100

    static eval script

---
+++ eval.py
@@ -0,0 +1,27 @@
+import datasets
+from jiwer import wer, cer
+from aaas.audio_utils.asr import inference_asr
+import re
+
+base = []
+predicted = []
+
+ds = (
+    datasets.load_dataset(
+        "mozilla-foundation/common_voice_11_0",
+        "de",
+        split="validation",
+        streaming=True,
+    )
+    ._resolve_features()
+    .cast_column("audio", datasets.features.Audio(sampling_rate=16000, decode=False))
+)
+
+for d in ds:
+    base_str = re.sub(r"[^\w\s]", "", d["sentence"].lower())
+    pred_str = inference_asr(d["audio"]["bytes"], "german", "large")
+    pred_str = re.sub(r"[^\w\s]", "", pred_str.lower())
+    base.append(base_str)
+    predicted.append(pred_str)
+
+    print(wer(base, predicted) * 100, cer(base, predicted) * 100)
