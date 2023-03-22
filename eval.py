6d75d398bebe46d2ca84933b15e9f6017075cc97
Florian Zimmermeister <flozi00.fz@gmail.com>
2023-03-22 21:43:38 +0100

    more semantic error rate calculation at eval

--- eval.py
+++ eval.py
@@ -4,9 +4,13 @@ from aaas.audio_utils.asr import inference_asr
 import re
 from text_to_num import alpha2digit
 from tqdm.auto import tqdm
+from unidecode import unidecode
+import diff_match_patch as dmp_module
 
 base = []
 predicted = []
+
+dmp = dmp_module.diff_match_patch()
 
 ds = datasets.load_dataset(
     "common_voice",
@@ -15,14 +19,16 @@ ds = datasets.load_dataset(
 ).cast_column("audio", datasets.features.Audio(sampling_rate=16000, decode=False))
 
 ds = (
-    ds.filter(lambda x: x["down_votes"] <= 0)
-    .filter(lambda x: x["up_votes"] <= 2)
+    ds.filter(lambda x: x["down_votes"] == 0)
+    .filter(lambda x: x["up_votes"] >= 2)
+    .filter(lambda x: len(x["sentence"]) >= 16)
+    .sort("up_votes", reverse=False)
     .cast_column("audio", datasets.features.Audio(sampling_rate=16000, decode=True))
 )
 
 for d in tqdm(ds):
     # normalize base transcription
-    base_str_orig = alpha2digit(d["sentence"], "de")
+    base_str_orig = unidecode(alpha2digit(d["sentence"], "de"))
     base_str = (
         re.sub(r"[^\w\s]", "", base_str_orig.strip())
         .lower()
@@ -38,7 +44,9 @@ for d in tqdm(ds):
         audio_data = d["audio"]["array"]
 
         # normalize prediction
-        pred_str_orig = alpha2digit(inference_asr(audio_data, "german", "large"), "de")
+        pred_str_orig = unidecode(
+            alpha2digit(inference_asr(audio_data, "german", "large"), "de")
+        )
         pred_str = (
             re.sub(r"[^\w\s]", "", pred_str_orig.strip())
             .lower()
@@ -49,24 +57,30 @@ for d in tqdm(ds):
             .replace("ß", "ss")
         )
 
+        diff_score = 0
+        diff = dmp.diff_main(base_str.replace(" ", ""), pred_str.replace(" ", ""))
+        dmp.diff_cleanupSemantic(diff)
+        for d in diff:
+            if d[0] != 0:
+                if len(d[1]) > 1:
+                    diff_score += 1
+
         # append to lists
-        base.append(
-            pred_str
-            if base_str.replace(" ", "") == pred_str.replace(" ", "")
-            else base_str
-        )
+        base.append(pred_str if diff_score == 0 else base_str)
         predicted.append(pred_str)
 
         # print results
-        if base_str.replace(" ", "") != pred_str.replace(" ", ""):
-            print(f"Base: {base_str}")
-            print(f"Pred: {pred_str}")
-            print(wer(base, predicted) * 100, cer(base, predicted) * 100)
+        if diff_score != 0:
+            diff = dmp.diff_main(base_str, pred_str)
+            dmp.diff_cleanupSemantic(diff)
+            print()
+            print(diff)
+            print("normalized", wer(base, predicted) * 100, cer(base, predicted) * 100)
             print()
     except Exception as e:
         print(e)
 
-    if len(base) > 100:
+    if len(base) > 100000:
         break
 
 print(wer(base, predicted) * 100, cer(base, predicted) * 100)
