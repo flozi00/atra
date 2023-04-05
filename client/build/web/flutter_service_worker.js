d1c2a1c6506a363ca7e680960662ddcd252d91a8
Florian Zimmermeister <flozi00.fz@gmail.com>
2023-04-05 11:04:29 +0200

    colorscheme

--- client/build/web/flutter_service_worker.js
+++ client/build/web/flutter_service_worker.js
@@ -21,10 +21,10 @@ const RESOURCES = {
 "icons/Icon-192.png": "41ea42964ca3db15103371652cfb8b99",
 "icons/icon-512-maskable.png": "3393925a0d62d0c39328a6743660d329",
 "icons/Icon-512.png": "e766c919bf2929dd4f22e732d9a96ee1",
-"index.html": "cfabdec740761ebfda4559e08b01192a",
-"/": "cfabdec740761ebfda4559e08b01192a",
+"index.html": "672e8ada97b01132869760b1fb47f35f",
+"/": "672e8ada97b01132869760b1fb47f35f",
 "main.dart.js": "d429ee74db6c932e9dedd0391cea4902",
-"manifest.json": "1dcd6ccb6211e207e63d77d251b83117",
+"manifest.json": "529317c6c07952ed5b23423e6da48ac6",
 "version.json": "e029f4818410e5d25961a81c424aea54"
 };
 
