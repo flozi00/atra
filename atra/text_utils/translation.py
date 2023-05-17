ef4e7f10a84b22e9446a138de446716019bd88c9
Florian Zimmermeister <flozi00.fz@gmail.com>
2023-05-17 14:55:56 +0200

    another fix

--- atra/text_utils/translation.py
+++ atra/text_utils/translation.py
@@ -12,18 +12,18 @@ def translate(text, src, dest) -> str:
     model, tokenizer = get_model_and_processor("universal", "translation")
     tokenizer.src_lang = src # type: ignore
     input_features = tokenizer(text, return_tensors="pt").input_ids
-    generated_tokens = inference_translate(model, input_features, tokenizer)
+    generated_tokens = inference_translate(model, input_features, tokenizer.get_lang_id(dest)) # type: ignore
     result = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
     return result[0]
 
 
 @timeit
-def inference_translate(model, input_features, tokenizer):
+def inference_translate(model, input_features, forced_bos_id):
     if torch.cuda.is_available():
         input_features = input_features.cuda()
     with torch.inference_mode():
         generated_tokens = model.generate(
-            inputs=input_features, forced_bos_token_id=tokenizer.get_lang_id(dest) # type: ignore
+            inputs=input_features, forced_bos_token_id=forced_bos_id # type: ignore
         )
 
     return generated_tokens
