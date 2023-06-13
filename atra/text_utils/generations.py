240c3a1f2956886d35ce9cde98805d1cbcd9c92a
flozi00 <flozi00.fz@gmail.com>
2023-06-13 14:34:10 +0200

    ngram repeating, min len smaller

--- atra/text_utils/generations.py
+++ atra/text_utils/generations.py
@@ -56,10 +56,11 @@ def do_generation(input, constraints: list[list[str]] = None, max_len = 512):
     generate_kwargs = dict(
         **input_ids,
         max_new_tokens=max_len,
-        min_new_tokens = int(max_len/4),
+        min_new_tokens = int(max_len/16),
         do_sample=False,
         num_beams=1,
-        temperature=0.1,
+        temperature=0.01,
+        no_repeat_ngram_size=3,
         use_cache = True,
     )
     if constraints is not None:
