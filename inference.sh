f6949a9dcef5857b96c2e419f8d3b17a13651ef9
flozi00 <flozi00.fz@gmail.com>
2023-06-27 13:03:50 +0200

    increase context

--- inference.sh
+++ inference.sh
@@ -4,4 +4,4 @@ quantize=gptq
 volume=$PWD/data # share a volume with the Docker container to avoid downloading weights every run
 
 docker pull ghcr.io/huggingface/text-generation-inference:latest
-docker run --gpus all --shm-size 1g -p 8080:80 -v $volume:/data ghcr.io/huggingface/text-generation-inference:latest --model-id $model #--quantize=$quantize
+docker run --gpus all --shm-size 1g -p 8080:80 -v $volume:/data ghcr.io/huggingface/text-generation-inference:latest --max_total_tokens 2048 --model-id $model #--quantize=$quantize
