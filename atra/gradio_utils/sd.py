1393b4b2c0fc824ff71f9afd437a4f976c3cac2a
flozi00 <flozi00.fz@gmail.com>
2023-07-17 18:52:08 +0200

    name api

--- atra/gradio_utils/sd.py
+++ atra/gradio_utils/sd.py
@@ -1,6 +1,18 @@
+import time
 from atra.gradio_utils.ui import GLOBAL_CSS, GET_GLOBAL_HEADER, launch_args
 from atra.image_utils.diffusion import generate_images
 import gradio as gr
+from gradio_client import Client
+
+client = Client(src="https://chat.atra.ai")
+
+
+def use_diffusion_ui(prompt, negatives):
+    job = client.submit(3, api_name="/sd")
+    while not job.done():
+        time.sleep(0.1)
+
+    return job.result()
 
 
 def build_diffusion_ui():
@@ -13,7 +25,9 @@ def build_diffusion_ui():
                 negatives = gr.Textbox(label="Negative Prompt")
             images = gr.Image()
 
-        prompt.submit(generate_images, inputs=[prompt, negatives], outputs=images)
+        prompt.submit(
+            generate_images, inputs=[prompt, negatives], outputs=images, api_name="sd"
+        )
         negatives.submit(generate_images, inputs=[prompt, negatives], outputs=images)
 
     ui.queue(concurrency_count=1, api_open=False)
