d5c8a6ecd0c6d8d0f8912242722bfa131543095c
flozi00 <flozi00.fz@gmail.com>
2023-08-25 17:18:30 +0000

    refactor to class ( chat )

--- atra/gradio_utils/chat.py
+++ atra/gradio_utils/chat.py
@@ -1,98 +1,23 @@
 import gradio as gr
 from atra.gradio_utils.ui import GET_GLOBAL_HEADER, launch_args
-from playwright.sync_api import sync_playwright
-import urllib.parse
 from atra.text_utils.prompts import (
     ASSISTANT_TOKEN,
     END_TOKEN,
     SYSTEM_PROMPT,
     SEARCH_PROMPT,
-    CLASSIFY_SEARCHABLE,
     USER_TOKEN,
     QUERY_PROMPT,
 )
-from sentence_transformers import SentenceTransformer, util
-import torch
+from atra.text_utils.assistant import Agent, Plugins
+from sentence_transformers import SentenceTransformer
 from huggingface_hub import InferenceClient
 import os
-import os
-import argilla as rg
-
-try:
-    rg.init(
-        api_url=os.environ.get("ARGILLA_API_URL"),
-        api_key=os.environ.get("ARGILLA_API_KEY"),
-        workspace="argilla",
-    )
-except Exception as e:
-    print(e)
 
 embedder = SentenceTransformer("intfloat/multilingual-e5-large", device="cpu")
 
 client = InferenceClient(model=os.environ.get("LLM", "http://127.0.0.1:8080"))
 
-
-def re_ranking(query: str, options: list):
-    """
-    Re-ranks a list of options based on their similarity to a given query.
-
-    Args:
-        query (str): The query to compare the options against.
-        options (list): A list of strings representing the options to be ranked.
-
-    Returns:
-        str: A string containing the top-ranked options that have a cosine similarity score greater than 0.7.
-    """
-    corpus = ["passage: " + o for o in options]
-    query = "query: " + query
-
-    filtered_corpus = []
-
-    corpus_embeddings = embedder.encode(corpus, convert_to_tensor=True)
-    query_embedding = embedder.encode(query, convert_to_tensor=True)
-
-    cos_scores = util.cos_sim(query_embedding, corpus_embeddings)[0]
-    top_results = torch.topk(cos_scores, k=20 if len(corpus) > 20 else len(corpus))
-
-    for score, idx in zip(top_results[0], top_results[1]):
-        if score > 0.7:
-            filtered_corpus.append(corpus[idx])
-
-    return "\n".join(filtered_corpus)
-
-
-def get_webpage_content_playwright(query):
-    """
-    Uses Playwright to launch a Chromium browser and navigate to a search engine URL with the given query.
-    Returns the filtered and re-ranked text content of the webpage.
-
-    Args:
-    - query (str): The search query to be used in the URL.
-
-    Returns:
-    - filtered (str): The filtered and re-ranked text content of the webpage.
-    """
-    url = (
-        "https://searx.be/search?categories=general&language=de&q="
-        + urllib.parse.quote(query)
-    )
-    with sync_playwright() as p:
-        browser = p.chromium.launch()
-        page = browser.new_page()
-        page.goto(url)
-        content = page.locator("body").inner_text()
-        browser.close()
-
-    content = content.split("\n")
-    filtered = ""
-    for co in content:
-        if len(co.split(" ")) > 5:
-            filtered += co + "\n"
-
-    filtered = re_ranking(query, filtered.split("\n"))
-
-    return filtered
-
+agent = Agent(client, embedder)
 
 def get_user_messages(history, message):
     """
@@ -147,77 +72,23 @@ def generate_history_as_string(history, message):
 
 
 def predict(message, chatbot):
-    """
-    1. Prepare the input prompt for the model.
-    2. Generate a response with the model.
-    3. Check whether the response is searchable or not.
-    4. If the response is searchable, generate a search query and search question.
-    5. Get the answer from the internet.
-    6. If the response is not searchable, generate a new response.
-    7. If the response is not searchable, yield the response.
-    8. If the response is searchable, yield the answer from the internet.
-    """
     input_prompt = generate_history_as_string(chatbot, message)
     user_messages = get_user_messages(chatbot, message)
-    searchable_answer = client.text_generation(
-        prompt=CLASSIFY_SEARCHABLE.replace("<|question|>", user_messages),
-        temperature=0.1,
-        stop_sequences=["\n", END_TOKEN],
-        max_new_tokens=3,
-    )
-    searchable = "Search" in searchable_answer
-    search_query_record = rg.Text2TextRecord(text=user_messages, prediction=[searchable_answer],)
-    rg.log(search_query_record, "plugin_record")
+    
+    plugin = agent.classify_plugin(user_messages)
 
-    text = ""
-    if searchable is True:
-        search_query = client.text_generation(
-            prompt=QUERY_PROMPT.replace("<|question|>", user_messages),
-            stop_sequences=["\n", END_TOKEN],
-        ).strip()
-        search_query_record = rg.Text2TextRecord(text=user_messages, prediction=[search_query],)
-        rg.log(search_query_record, "search_query_record")
+    if plugin == Plugins.SEARCH:
+        search_query = agent.generate_search_query(user_messages)
+        search_question = agent.generate_search_question(user_messages)
+        options = agent.get_webpage_content_playwright(search_query)
 
-        search_question = client.text_generation(
-            prompt=SEARCH_PROMPT.replace("<|question|>", user_messages),
-            stop_sequences=["\n", END_TOKEN],
-        ).strip()
-        search_question_record = rg.Text2TextRecord(text=user_messages, prediction=[search_question],)
-        rg.log(search_question_record, "search_question_record")
-
-        text += "```\nSearch query: " + search_query + "\n```\n\n"
-        options = get_webpage_content_playwright(search_query)
-
-        QA_Prompt = USER_TOKEN + options + "\nQuestion: " + "\n\n" + search_question + "\n\nAnswer in german plain text:" + END_TOKEN + ASSISTANT_TOKEN
-        answer = client.text_generation(
-            prompt=QA_Prompt,
-            max_new_tokens=512,
-            temperature=0.1,
-            stop_sequences=["<|", END_TOKEN],
-        )
-        text += answer
-        yield text.replace("<|", "")
-        record = rg.Text2TextRecord(
-            text=QA_Prompt,
-            prediction=[answer],
-        )
-        rg.log(record, "qa_record")
+        answer = agent.do_qa(search_question, options)
+        for text in answer:
+            yield text
     else:
-        for token in client.text_generation(
-            prompt=input_prompt,
-            max_new_tokens=512,
-            temperature=0.6,
-            stop_sequences=["<|", END_TOKEN],
-            stream=True,
-        ):
-            text += token
-            yield text.replace("<|", "")
-        
-        record = rg.Text2TextRecord(
-            text=input_prompt,
-            prediction=[text],
-        )
-        rg.log(record, "chat_record")
+        answer = agent.custom_generation(input_prompt)
+        for text in answer:
+            yield text
 
 
 def build_chat_ui():
