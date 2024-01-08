f83a1534d829cae43c17a7518a674ef19f224d00
flozi00 <you@example.com>
2024-01-08 15:14:46 +0100

    clean asr pipe

--- atra/audio_utils/asr.py
+++ atra/audio_utils/asr.py
@@ -11,11 +11,6 @@ from transformers import AutoModelForCausalLM
 
 warnings.filterwarnings(action="ignore")
 
-if torch.cuda.is_available():
-    GPU_NAME = torch.cuda.get_device_name(0).split(" ")[-1]
-else:
-    GPU_NAME = "CPU"
-
 assistant_model = AutoModelForCausalLM.from_pretrained(
     os.getenv("SPEC_ASSISTANT_MODEL", "flozi00/distilwhisper-german-v1"),
     torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32,
@@ -27,38 +22,27 @@ assistant_model = AutoModelForCausalLM.from_pretrained(
 if torch.cuda.is_available():
     assistant_model = assistant_model.to("cuda:0")
 
-# try:
-#    assistant_model = torch.compile(assistant_model, mode="max-autotune")
-# except Exception:
-#    pass
-
 pipe = pipeline(
     "automatic-speech-recognition",
     os.getenv("ASR_MODEL", "primeline/whisper-large-v3-german"),
     torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32,
     model_kwargs={
         "load_in_4bit": True,
-        # "use_flash_attention_2": "A" in GPU_NAME or "H" in GPU_NAME or "L" in GPU_NAME,
         "attn_implementation": "sdpa",
     },
-    batch_size=4,
+    batch_size=1,
 )
 pipe.model.eval()
-
-# try:
-#    pipe.model = torch.compile(pipe.model, mode="max-autotune")
-# except Exception:
-#    pass
 
 
 def speech_recognition(data, language, progress=gr.Progress()) -> str:
     if data is None:
         return ""
 
-    progress.__call__(progress=0.8, desc="Transcribing Audio")
+    progress.__call__(progress=0.7, desc="Transcribing Audio")
     transcription = inference_asr(pipe=pipe, data=data, language=language)
 
-    progress.__call__(progress=0.9, desc="Converting to Text")
+    progress.__call__(progress=0.8, desc="Converting to Text")
     try:
         transcription = alpha2digit(
             text=transcription, lang=WHISPER_LANG_MAPPING[language]
