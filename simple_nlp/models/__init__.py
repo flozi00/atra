1358de1a9cbbeb153d1410112e0411b442c5b844
flozi00 <flozi00.fz@gmail.com>
2024-03-19 19:36:54 +0100

    add asr training script

---
+++ simple_nlp/models/__init__.py
@@ -0,0 +1,76 @@
+from transformers import AutoConfig
+from transformers import (
+    AutoProcessor,
+    AutoModelForSpeechSeq2Seq,
+    Wav2Vec2BertForCTC,
+)
+
+
+def get_model(
+    model_name: str,
+    processor_name: str = None,
+    use_flash_v2=False,
+    vocab_size=None,
+    processor=None,
+):
+    kwargs = {}
+
+    # get the config of the base model and extract the model type from it
+    conf = AutoConfig.from_pretrained(
+        pretrained_model_name_or_path=model_name,
+        trust_remote_code=True,
+    )
+
+    ctc_model = False
+    keys = ["wav2vec", "w2v"]
+    for key in keys:
+        if key in model_name:
+            ctc_model = True
+            conf.attention_dropout=0.0
+            conf.hidden_dropout=0.0
+            conf.feat_proj_dropout=0.0
+            conf.mask_time_prob=0.0
+            conf.layerdrop=0.0
+            conf.ctc_loss_reduction="mean"
+            conf.add_adapter = True
+            break
+    model_class = AutoModelForSpeechSeq2Seq if ctc_model is False else Wav2Vec2BertForCTC
+    tok_class = AutoProcessor
+
+
+    if processor is None:
+        processor = tok_class.from_pretrained(
+            model_name if processor_name is None else processor_name,
+            legacy=False,
+            trust_remote_code=True,
+        )
+
+    if use_flash_v2:
+        kwargs["attn_implementation"] = "flash_attention_2"
+    #else:
+    #    kwargs["attn_implementation"] = "sdpa"
+    
+    if vocab_size is not None:
+        conf.vocab_size = vocab_size
+        kwargs["ignore_mismatched_sizes"]=True
+
+    # load the pre-trained model and check if its 8-bit compatible
+    model = model_class.from_pretrained(
+        model_name,
+        config=conf,
+        **kwargs,
+    )
+
+    try:
+        if processor.pad_token is None:
+            processor.pad_token = processor.eos_token
+            print("Setting pad token to eos token")
+    except Exception:
+        if processor.tokenizer.pad_token is None:
+            processor.tokenizer.pad_token = processor.tokenizer.eos_token
+            print("Setting pad token to eos token")
+
+
+    model = model.train()
+
+    return model, processor
---
+++ simple_nlp/train/__init__.py
@@ -0,0 +1,104 @@
+
+from accelerate import Accelerator
+import warnings
+from tqdm.auto import tqdm
+from torch.optim.lr_scheduler import ExponentialLR
+from bitsandbytes.optim import PagedAdam
+import os
+
+ACCUMULATION_STEPS = int(os.getenv("ACCUMULATION_STEPS", 16))
+TOKEN = os.getenv("HF_TOKEN", None)
+EPOCHS = int(os.getenv("EPOCHS", 1))
+SAVE_STEPS = int(os.getenv("SAVE_STEPS", 1000))
+LR= float(os.getenv("LR", 1e-4))
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group["lr"]
+
+
+def start_training(
+    model,
+    processor,
+    dloader,
+    OUT_MODEL,
+    callback=None,
+):
+    accelerator = Accelerator(
+        log_with="wandb",
+        gradient_accumulation_steps=ACCUMULATION_STEPS,
+        mixed_precision="fp16",
+    )
+    accelerator.init_trackers("huggingface")
+
+    model.train()
+
+    optim = PagedAdam(model.parameters(), lr=LR)
+
+    scheduler = ExponentialLR(optim, gamma=0.9995)
+    model, optim, dloader, scheduler = accelerator.prepare(
+        model, optim, dloader, scheduler
+    )
+
+    if callback is not None:
+        eval_ = callback()
+        if eval_ is not None:
+            accelerator.log({"eval_metric": eval_}, step=0)
+
+    def do_save_stuff():
+        if callback is not None:
+            eval_ = callback()
+            if eval_ is not None:
+                accelerator.log({"eval_metric": eval_}, step=index - 1)
+        model.save_pretrained(
+            OUT_MODEL,
+            save_function=accelerator.save,
+            state_dict=accelerator.get_state_dict(model),
+            safe_serialization=False,
+        )
+        processor.save_pretrained(OUT_MODEL)
+
+        try:
+            model.push_to_hub(
+                OUT_MODEL,
+                save_function=accelerator.save,
+                state_dict=accelerator.get_state_dict(model),
+                safe_serialization=False,
+                token=TOKEN,
+            )
+            processor.push_to_hub(OUT_MODEL, token=TOKEN)
+        except Exception as e:
+            warnings.warn(f"Could not push to hub: {e}")
+
+    index = 0
+    for epoch in range(EPOCHS):
+        for data in (pbar := tqdm(dloader)):
+            if index / ACCUMULATION_STEPS % SAVE_STEPS == 0 and index != 0:
+                do_save_stuff()
+
+            optim.zero_grad()
+            with accelerator.accumulate(model), accelerator.autocast():
+                data = {k: v.to(model.device) for k, v in data.items()}
+                output = model(return_dict=True, **data)
+                loss = output.loss
+                accelerator.backward(loss)
+
+                if index % ACCUMULATION_STEPS == 0:
+                    pbar.set_description(
+                        f"Loss: {loss} LR: {get_lr(optim.optimizer)}",
+                        refresh=True,
+                    )
+                    accelerator.log(
+                        values={
+                            "training_loss": loss,
+                        },
+                        step=int(index / ACCUMULATION_STEPS),
+                    )
+                if accelerator.sync_gradients:
+                    accelerator.clip_grad_value_(model.parameters(), 0.7)
+                optim.step()
+                scheduler.step()
+
+            index += 1
+        do_save_stuff()
+    do_save_stuff()
