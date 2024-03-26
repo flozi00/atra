5f8493a6a062db71404feae81c01f4171c726009
flozi00 <flozi00.fz@gmail.com>
2024-03-26 17:33:51 +0100

    ttf fooocus

--- run_fooocus.sh
+++ run_fooocus.sh
@@ -14,6 +14,6 @@ echo "All files are downloaded, now running the server"
 
 rm /Fooocus/config.txt
 
-cp -f /atra-server/config.txt /Fooocus/config.txt
+cp -f /atra-server/atra/utilities/config.txt /Fooocus/config.txt
 
 python3 /Fooocus/entry_with_update.py --listen --always-gpu --vae-in-fp16 --unet-in-fp8-e4m3fn --clip-in-fp8-e4m3fn --port 7861
