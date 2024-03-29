8ab7324beed02e06c427706c93241846ce6939a4
flozi00 <flozi00.fz@gmail.com>
2024-03-29 21:45:26 +0100

    fix autotrain

--- run_autotrain.sh
+++ run_autotrain.sh
@@ -1,3 +1,3 @@
 pip install uvicorn
-pip install autotrain-advanced --no-deps --upgrade
+pip install autotrain-advanced --upgrade
 python3 -m uvicorn autotrain.app:app --host 0.0.0.0 --port 5001 --reload --workers 4
