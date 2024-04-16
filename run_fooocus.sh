8d1ce2c567bd23849ecda4f60ecfadb9116997ac
Florian Zimmermeister <flozi00.fz@gmail.com>
2024-04-16 13:11:41 +0200

    Update run_fooocus.sh

--- run_fooocus.sh
+++ run_fooocus.sh
@@ -16,4 +16,11 @@ rm /Fooocus/config.txt
 
 cp -f /atra-server/atra/utilities/config.txt /Fooocus/config.txt
 
-python3 /Fooocus/entry_with_update.py --listen --always-gpu --vae-in-fp16 --unet-in-fp8-e4m3fn --clip-in-fp8-e4m3fn --port 7861
+cmd="python3 /Fooocus/entry_with_update.py --listen --always-gpu --vae-in-fp16 --unet-in-fp8-e4m3fn --clip-in-fp8-e4m3fn"
+
+if [ -n "$SERVER_PORT" ]; then
+  cmd="$cmd --port $SERVER_PORT"
+fi
+
+echo "Running command: $cmd"
+$cmd
