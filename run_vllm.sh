5f752431a4c97d0371913fb9c382c08492f02ee8
Florian Zimmermeister <flozi00.fz@gmail.com>
2024-05-23 17:43:28 +0200

    Update run_vllm.sh

--- run_vllm.sh
+++ run_vllm.sh
@@ -5,7 +5,7 @@
 # 3. run the command
 
 # 1. create base string for the command
-cmd="python3 -m vllm.entrypoints.openai.api_server --use-v2-block-manager --enforce-eager --ngram-prompt-lookup-max 5"
+cmd="python3 -m vllm.entrypoints.openai.api_server --use-v2-block-manager --enforce-eager --ngram-prompt-lookup-max 5 --enable-lora"
 
 # 2. check if env vars exists and add them to the command
 # check for --host, --port, --quantization, --model, --kv-cache-dtype, --gpu-memory-utilization, --swap-space, --tensor-parallel-size
