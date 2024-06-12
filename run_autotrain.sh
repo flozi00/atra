5bccf09fa137df6a3f5910ed893b9939b52ddb4b
Florian Zimmermeister <flozi00.fz@gmail.com>
2024-06-12 10:10:07 +0200

    Update run_autotrain.sh

--- run_autotrain.sh
+++ run_autotrain.sh
@@ -1,3 +1,4 @@
 pip install uvicorn
 pip install autotrain-advanced --upgrade
+pip install --force-reinstall nvidia-ml-py nvitop
 python3 -m uvicorn autotrain.app.app:app --host 0.0.0.0 --port 5001 --reload --workers 4
