a668f90e40606a73ed174619e69180ac52f084b0
Florian Zimmermeister <flozi00.fz@gmail.com>
2024-07-16 09:38:30 +0200

    Update run_vllm.sh

--- run_vllm.sh
+++ run_vllm.sh
@@ -5,42 +5,7 @@
 # 3. run the command
 
 # 1. create base string for the command
-cmd="python3 -m vllm.entrypoints.openai.api_server --enforce-eager --ngram-prompt-lookup-max 3"
-
-# 2. check if env vars exists and add them to the command
-# check for --host, --port, --quantization, --model, --kv-cache-dtype, --gpu-memory-utilization, --swap-space, --tensor-parallel-size
-
-if [ -n "$VLLM_HOST" ]; then
-  cmd="$cmd --host $VLLM_HOST"
-fi
-
-if [ -n "$VLLM_PORT" ]; then
-  cmd="$cmd --port $VLLM_PORT"
-fi
-
-if [ -n "$VLLM_QUANTIZATION" ]; then
-  cmd="$cmd --quantization $VLLM_QUANTIZATION"
-fi
-
-if [ -n "$VLLM_MODEL" ]; then
-  cmd="$cmd --model $VLLM_MODEL"
-fi
-
-if [ -n "$VLLM_KV_CACHE_DTYPE" ]; then
-  cmd="$cmd --kv-cache-dtype $VLLM_KV_CACHE_DTYPE"
-fi
-
-if [ -n "$VLLM_GPU_MEMORY_UTILIZATION" ]; then
-  cmd="$cmd --gpu-memory-utilization $VLLM_GPU_MEMORY_UTILIZATION"
-fi
-
-if [ -n "$VLLM_SWAP_SPACE" ]; then
-  cmd="$cmd --swap-space $VLLM_SWAP_SPACE"
-fi
-
-if [ -n "$VLLM_TENSOR_PARALLEL_SIZE" ]; then
-  cmd="$cmd --tensor-parallel-size $VLLM_TENSOR_PARALLEL_SIZE"
-fi
+cmd="python3 -m vllm.entrypoints.openai.api_server "
 
 if [ -n "$VLLM_ADDITIONAL_ARGS" ]; then
   cmd="$cmd $VLLM_ADDITIONAL_ARGS"
