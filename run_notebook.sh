f80f435ebb81f0184cd7ccbde6114bc496cdf767
Florian Zimmermeister <flozi00.fz@gmail.com>
2024-09-15 18:47:07 +0200

    Update run_notebook.sh

--- run_notebook.sh
+++ run_notebook.sh
@@ -1,2 +1,2 @@
 pip install jupyterlab jupyterlab_widgets ipywidgets --upgrade
-mkdir -p /workdir && cd /workdir && jupyter lab --ip 0.0.0.0 --no-browser --port=8888 --allow-root
+mkdir -p /workdir && cd /workdir && jupyter lab --ip 0.0.0.0 --no-browser --port=8888 --allow-root --notebook-dir=/workdir/
