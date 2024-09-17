fb94bbfb00d63a7e8d84e415326a9cf036c80270
Florian Zimmermeister <flozi00.fz@gmail.com>
2024-09-17 17:40:45 +0200

    Update run_notebook.sh

--- run_notebook.sh
+++ run_notebook.sh
@@ -1,2 +1,3 @@
+pip install pyright python-language-server python-lsp-server --upgrade
 pip install jupyterlab jupyterlab_widgets ipywidgets --upgrade
 mkdir -p /workdir && cd /workdir && jupyter lab --ip 0.0.0.0 --no-browser --port=8888 --allow-root --notebook-dir=/workdir/
